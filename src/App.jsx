7efdd50c76f96617f600dad0e92d9fd8260760ed
Abdo222000 <abbasebrahiem123@gmail.com>
2025-12-11 10:24:42 +0200

    added gitignore and removed node_modules from repo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AbouUs from './components/AboutUs';
 import Hero from './components/Hero';
 import Home from './components/Home';
 import Navbar from "./components/Navbar";
-import View from './components/view';
+import View from './components/View';
 import Project from './components/projects';
 import ReviewsSlider from './components/reviews';
 
