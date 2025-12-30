f6901285b18c6ec69ee622a202afa9106964002e
mai yasser <maiyasser287@gmail.com>
2025-12-30 05:11:52 +0200

    blog&blog details

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,16 +1,41 @@
 import "./Project.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Project({ limit }) {
   const projects = [
-    { id: 1, img: "/ring.jpg", name: "M & A", title: "Let's go to join our wedding" },
-    { id: 2, img: "/ring.jpg", name: "M & A", title: "Special Wedding Day" },
-    { id: 3, img: "/ring.jpg", name: "M & A", title: "Luxury Wedding Party" },
-    { id: 4, img: "/ring.jpg", name: "M & A", title: "Forever Together" },
-    { id: 5, img: "/ring.jpg", name: "M & A", title: "Let's go to join our wedding" },
-    { id: 6, img: "/ring.jpg", name: "M & A", title: "Let's go to join our wedding" },
-    { id: 7, img: "/ring.jpg", name: "M & A", title: "Let's go to join our wedding" },
-    { id: 8, img: "/ring.jpg", name: "M & A", title: "Let's go to join our wedding" },
+    {
+      id: 1,
+      img: "/photo-session8.jpg",
+      name: "M & A",
+      title: "Let's go to join our wedding",
+    },
+    { id: 2, img: "/photo-session.jpg", name: "M & A", title: "Special Wedding Day" },
+    { id: 3, img: "/photo-session2.jpg", name: "M & A", title: "Luxury Wedding Party" },
+    { id: 4, img: "/photo-session6.jpg", name: "M & A", title: "Forever Together" },
+    {
+      id: 5,
+      img: "/photo-session4.jpg",
+      name: "M & A",
+      title: "Let's go to join our wedding",
+    },
+    {
+      id: 6,
+      img: "/photo-session7.jpg",
+      name: "M & A",
+      title: "Let's go to join our wedding",
+    },
+    {
+      id: 7,
+      img: "/photo-session5.jpg",
+      name: "M & A",
+      title: "Let's go to join our wedding",
+    },
+    {
+      id: 8,
+      img: "/photo-session3.jpg",
+      name: "M & A",
+      title: "Let's go to join our wedding",
+    },
   ];
 
   const displayedProjects = limit ? projects.slice(0, limit) : projects;
@@ -35,29 +60,35 @@ function Project({ limit }) {
           {displayedProjects.map((project) => (
             <div key={project.id} className="item col-3">
               <div className="portfolio-one__item">
-                <div className="portfolio-one__thumb">
+                <Link to="/project-details" className="portfolio-one__thumb">
                   <img src={project.img} alt={project.title} loading="lazy" />
-                </div>
-
+                </Link>
                 <div className="portfolio-one__hover">
                   <div className="portfolio-one__hover-bottom">
                     <div className="portfolio-one__cats">
-                      <span>{project.name}</span>
+                      <Link to="/project-details">
+                        <span>{project.name}</span>
+                      </Link>
                     </div>
-                    <h3 className="portfolio-one__title">{project.title}</h3>
+                    <Link to="/project-details">
+                      <h3 className="portfolio-one__title">{project.title}</h3>
+                    </Link>
                   </div>
                 </div>
               </div>
             </div>
           ))}
         </div>
+
         {/* Slider End */}
 
-        <div className="button">
-          <Link to="/services" className="more-btn">
-            <span className="-btn">Discover More</span>
-          </Link>
-        </div>
+        {limit && (
+          <div className="button">
+            <Link to="/projects" className="more-btn">
+              <span className="-btn">Discover More</span>
+            </Link>
+          </div>
+        )}
       </div>
     </section>
   );
